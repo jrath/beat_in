dc8e86bcc1bcbfea4c4d9db77e888ca4004bf71f
jrath <jrath@jrath-desktop.(none)>
2010-06-15 12:34:50 +0530

    escaping html in beats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def display_beat_content(beat)
-    content = beat.content
+    content = h beat.content
     if beat.source_beat # beat has a source so it is a comment
       replied_to, reply = beat.content.split(' ',2)
       content = "@<a href=\"#{profile_user_url(:id => beat.source_beat.user_id)}\">#{h replied_to[1..-1]}</a> " + h(reply)
