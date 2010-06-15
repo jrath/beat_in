dceea756c4670b25727d540c800038288f60ac2a
jrath <jrath@jrath-desktop.(none)>
2010-06-15 08:33:36 +0530

    logging runtimes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   include AuthenticatedSystem
-
+  after_filter :log_runtimes
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  private
+  def log_runtimes
+    return if response.headers['Status'] == '304 Not Modified' || !(response.body and response.body.respond_to?(:sub!))
+   
+    db_runtime = ((0 + (@db_rt_before_render || 0) + (@db_rt_after_render || 0))* 1000).truncate
+    rendering_runtime = ((@rendering_runtime || 0)* 1000).truncate
+    total_runtime = ((@total_runtime || 0)* 1000).truncate
+    logger.info "total : #{total_runtime} ms, db : #{db_runtime} ms, render : #{rendering_runtime} ms"
+  
+  end
 end
